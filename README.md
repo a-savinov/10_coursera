94f8c13ac48c98be70fd02706e4be60deeabe658
Алексей Савинов <belsat.net@gmail.com>
2017-10-10 13:22:39 +0300

    collect info about Coursera courses

--- README.md
+++ README.md
@@ -1,6 +1,35 @@
 # Coursera Dump
 
-[TODO. There will be project description]
+The program collects data from N courses in Coursera and saves it to a file
+
+### How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+### How to use
+```bash
+
+$ python3 coursera.py -h
+usage: coursera.py [-h] [-a AMOUNT] -f FILE
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -a AMOUNT, --amount AMOUNT
+                        Amount of Coursera courses to view
+  -f FILE, --file FILE  Path to output Excel .xlsx file
+```
+
+### Sample output
+```bash
+$ python coursera.py -a 20 -f sample.xlsx
+
+30% (6 of 20) |#######                   | Elapsed Time: 0:00:09 ETA:  0:00:21
+```
+Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
 
 # Project Goals
 
--- coursera.py
+++ coursera.py
@@ -1,38 +1,66 @@
 import requests
 from bs4 import BeautifulSoup
+from openpyxl import Workbook
 import random
+import progressbar
+import argparse
 
 
 def get_courses_list(courses_amount):
-    xml_feed = requests.get('https://www.coursera.org/sitemap~www~courses.xml').content
+    xml_feed = requests.get(
+        'https://www.coursera.org/sitemap~www~courses.xml').content
     soup = BeautifulSoup(xml_feed, 'lxml')
     urls = soup.find_all('loc')
-    random_urls_list = [random.choice(urls).text for i in range(courses_amount)]
-    return random_urls_list
-
+    random_urls = [random.choice(urls).text for i in range(courses_amount)]
+    return random_urls
 
 
 def get_course_info(course_url):
-    '''
-    Зайти на страницу курса и вытащить оттуда название, язык, ближайшую дату начала, количество недель и среднюю оценку.
-    '''
-    course_info = requests.get(course_url).text
-    soup = BeautifulSoup(course_info)
+    course_info = requests.get(course_url).content.decode('utf-8')
+    soup = BeautifulSoup(course_info, 'lxml')
     course_name = soup.find('h1', {'class': 'title display-3-text'}).text
     course_lang = soup.find('div', {'class': 'rc-Language'}).text
-    course_rating = soup.find('div', {'class': 'ratings-text bt3-hidden-xs'}).text
-    course_duration = soup.find('td', {'class': 'td-data'}).text
-    return course_info
+    course_start_date = soup.find(
+        'div', {'class': 'startdate rc-StartDateString caption-text'}).text
+    if soup.find('div', {'class': 'ratings-text bt3-visible-xs'}):
+        course_rating = soup.find(
+            'div', {'class': 'ratings-text bt3-visible-xs'}).text
+    else:
+        course_rating = 'Not rated yet'
+    course_duration = len(soup.findAll('div', {'class': 'week'}))
+    return [course_name, course_lang, course_start_date, course_duration,
+            course_rating]
 
 
-def output_courses_info_to_xlsx(filepath):
-    pass
+def output_courses_info_to_xlsx(courses_info, filepath):
+    wb = Workbook()
+    ws = wb.active
+    table_title = ['Course name', 'Language ', 'Start date', 'Duration (week)',
+                   'Rating']
+    ws.append(table_title)
+    for course in courses_info:
+        ws.append(course)
+    wb.save(filepath)
+
+
+def get_input_argument_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--amount', required=False, default=20, type=int,
+                        help='Amount of Coursera courses to view')
+    parser.add_argument('-f', '--file', required=True,
+                        help='Path to output Excel .xlsx file')
+    return parser
 
 
 if __name__ == '__main__':
-    courses_list = get_courses_list(10)
-    print(courses_list)
-    for course in courses_list:
-        course_info = get_course_info(course)
-
-
+    parser = get_input_argument_parser()
+    args = parser.parse_args()
+    courses_amount = args.amount
+    filepath = args.file
+    courses_list = get_courses_list(courses_amount)
+    courses_info = []
+    bar = progressbar.ProgressBar(max_value=courses_amount)
+    for index, course in enumerate(courses_list):
+        courses_info.append(get_course_info(course))
+        bar.update(index+1)
+    output_courses_info_to_xlsx(courses_info, filepath)
